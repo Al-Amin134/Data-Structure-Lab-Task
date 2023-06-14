8a0400347c8a8ed2b7f445a9b5157a1a64872f2c
Md.Al-Amin Babu <112581855+Al-Amin134@users.noreply.github.com>
2023-06-14 14:04:38 +0600

    Update tree with its shape.cpp

--- Tree/tree with its shape.cpp
+++ Tree/tree with its shape.cpp
@@ -1,27 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
-void insert_tree(int value,int p,int prev)
-{
-    cout<<"/4t";
+int tree[50]= {0};
+int table_size = 8;
 
+
+void print(int i,int f,int p,int c)
+{
+
+    if((tree[i]!=NULL)&&(c==1))
+    {
+        while(f--)
+        {
+        cout<<" ";
+        }
+        cout<<tree[i];
+        return;
+    }
+    else if(tree[i]!=NULL)
+    {
+        while(p>0)
+        {
+            cout<<" ";
+            p--;
+        }
+
+        cout<<tree[i];
+    }
+    else{
+        while(p--){
+            cout<<" ";
+        }
+    }
 
 }
 int main()
 {
-    int n,value;
-    cin>>n;
-    int c=1;
-    int p=pow(2,n)-1;
-    while(c<=n){
-            int m=c;
-    int prev=p;
-    p=p/2;
-    while(m--){
-            cin>>value;
-    insert_tree(p,value,prev);
+
+    for(int i=0; i<table_size; i++)
+    {
+        cin>>tree[i];
+    }
+    int f = pow(2,4)-1;
+    int c=1,p=0;
+    for(int i=0; i<table_size; i++)
+    {
+        p = f;
+        f=f/2;
+        for(int j=1; j<=c; j++)
+        {
+            if(j>1)i++;
+            print(i,f,p,j);
+
+        }
+        c*=2;
+        cout<<endl;
     }
 
-    c++;
-      cout<<endl;
-    }
 }
+//input: 38 58 55 20 30 0 97 35
+/*output:
+
+       38
+   58       55
+ 20   30      97
+35
+
